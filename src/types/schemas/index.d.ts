db49174d0035190ef21a1ca4468a289c314c9ab8
hoXyy <hoxtf2@outlook.com>
2021-08-17 21:28:26 +0200

    Update index.d.ts

--- src/types/schemas/index.d.ts
+++ src/types/schemas/index.d.ts
@@ -3,5 +3,4 @@ export * from './autoUpdateTotal';
 export * from './bids';
 export * from './countdown';
 export * from './countdownRunning';
-export * from './exampleReplicant';
 export * from './total';
