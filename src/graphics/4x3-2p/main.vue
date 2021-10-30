cd7ac6f91976c9bcc9af4b61c0909704f63e09da
hoXyy <hoxtf2@outlook.com>
2021-10-30 18:29:08 +0200

    Add 4x3 2P layout

---
+++ src/graphics/4x3-2p/main.vue
@@ -0,0 +1,205 @@
+<template>
+    <div id="container">
+        <img id="Background" src="../img/layouts/standard_2.png" />
+
+        <div v-if="activeRun" id="runners">
+            <div v-if="activeRun.teams[0]" id="team1">
+                <template v-for="player in activeRun.teams[0].players">
+                    <player
+                        :key="player.id"
+                        class="Player"
+                        :cycle="nameCycle"
+                        :player="player"
+                    />
+                </template>
+                <commentator-list
+                    :commentators="commentators"
+                    v-if="numRunners < 3 && commentators.amount > 0"
+                    id="commentators"
+                />
+            </div>
+            <div v-if="activeRun.teams[1]" id="team2">
+                <template v-for="player in activeRun.teams[1].players">
+                    <player
+                        :key="player.id"
+                        class="Player"
+                        :cycle="nameCycle"
+                        :player="player"
+                    />
+                </template>
+                <reader-name
+                    :reader="reader"
+                    v-if="numRunners < 4 && reader"
+                    id="reader"
+                />
+            </div>
+        </div>
+        <run-info
+            id="RunInfo"
+            v-if="activeRun"
+            :run="activeRun"
+            :maxTitleSize="43"
+        />
+        <timer id="timer" />
+        <sponsors-view id="sponsors" :sponsors="sponsors" />
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator'
+    import type { RunDataActiveRun } from 'nodecg/bundles/nodecg-speedcontrol/src/types/schemas'
+    import type {
+        NameCycle,
+        Commentators,
+        Reader,
+    } from '@gsps-layouts/types/schemas'
+    import type { Asset } from '@gsps-layouts/types'
+    import { Getter } from 'vuex-class'
+    import Timer from '../_misc/components/Timer.vue'
+    import RunInfo from '../_misc/components/RunInfo.vue'
+    import Player from '../_misc/components/Player.vue'
+    import CommentatorList from '../_misc/components/Commentator.vue'
+    import ReaderName from '../_misc/components/Reader.vue'
+    import SponsorsView from '../_misc/components/Sponsors.vue'
+
+    @Component({
+        components: {
+            Timer,
+            RunInfo,
+            Player,
+            CommentatorList,
+            ReaderName,
+            SponsorsView,
+        },
+    })
+    export default class extends Vue {
+        @Getter readonly activeRun!: RunDataActiveRun
+        @Getter readonly nameCycle!: NameCycle
+        @Getter readonly commentators!: Commentators
+        @Getter readonly reader!: Reader
+        @Getter readonly sponsors!: Asset[]
+        data() {
+            return {
+                numRunners: 0,
+            }
+        }
+
+        mounted() {
+            this.activeRun!.teams.forEach((team: any) => {
+                this.$data.numRunners += team.players.length
+            })
+
+            this.$watch(
+                function () {
+                    return this.activeRun
+                },
+                function () {
+                    this.$data.numRunners = 0
+                    this.activeRun!.teams.forEach((team: any) => {
+                        this.$data.numRunners += team.players.length
+                    })
+                }
+            )
+        }
+    }
+</script>
+
+<style>
+    @import url('../css/styles.css');
+
+    #container {
+        text-align: center;
+    }
+
+    #Background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: -1;
+    }
+
+    #timer {
+        left: 686.4px;
+        bottom: 15px;
+        height: 78px;
+        width: 547.2px;
+        font-size: 67.2px;
+        z-index: 2;
+        position: absolute;
+    }
+
+    #bottomLeft {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 583px;
+        width: 578px;
+        height: 430px;
+    }
+
+    html {
+        display: block;
+        width: 1920px;
+        height: 1014px;
+    }
+
+    #RunInfo {
+        position: absolute;
+        bottom: 35px;
+        width: 634.8px;
+        height: 160px;
+    }
+
+    #runners {
+        top: 720px;
+        width: 100%;
+        font-size: 28.8px;
+        left: -11px;
+        position: absolute;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .Player {
+        position: relative;
+        margin-bottom: 0px;
+        width: 640px;
+    }
+
+    #team1 {
+        right: 1280px;
+        align-self: flex-start;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #team2 {
+        left: 1280px;
+        align-self: flex-start;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #commentators {
+        position: relative;
+        margin-bottom: 0px;
+        width: 640px;
+    }
+
+    #reader {
+        position: relative;
+        margin-bottom: 0px;
+        width: 640px;
+    }
+
+    #sponsors {
+        position: absolute;
+        bottom: 24px;
+        right: 0px;
+        width: 640px;
+        height: 180px;
+        /* 		background-color: aqua; */
+    }
+</style>
--- src/graphics/_misc/components/Player.vue
+++ src/graphics/_misc/components/Player.vue
@@ -1,43 +1,37 @@
 <template>
-    <div
-        :style="{
-            'background-color': '#3A008B',
-            color: 'white',
-            height: '36px',
-            'font-size': '24px',
-            width: '100%',
-        }"
-    >
-        <transition name="fade">
-            <span
-                v-if="cycle === 0"
-                :style="{
-                    'margin-top': '-2px',
-                    position: 'absolute',
-                    left: '0',
-                    width: '100%',
-                }"
-            >
-                <b>{{ player.name || '?' }}</b>
-            </span>
-        </transition>
-        <transition name="fade">
-            <span
-                v-if="cycle === 1"
-                :style="{
-                    'margin-top': '-2px',
-                    position: 'absolute',
-                    left: '0',
-                    width: '100%',
-                }"
-            >
-                <i
-                    :style="{ 'font-size': '20px', 'margin-right': '8px' }"
-                    class="fab fa-twitch"
-                ></i
-                ><b>{{ player.social.twitch || '?' }}</b>
-            </span>
-        </transition>
+    <div>
+        <div class="nameplate">
+            <transition name="fade">
+                <span
+                    v-if="cycle === 0"
+                    :style="{
+                        'margin-top': '-2px',
+                        position: 'absolute',
+                        left: '0',
+                        width: '100%',
+                    }"
+                >
+                    <b>{{ player.name || '?' }}</b>
+                </span>
+            </transition>
+            <transition name="fade">
+                <span
+                    v-if="cycle === 1"
+                    :style="{
+                        'margin-top': '-2px',
+                        position: 'absolute',
+                        left: '0',
+                        width: '100%',
+                    }"
+                >
+                    <i
+                        :style="{ 'font-size': '20px', 'margin-right': '8px' }"
+                        class="fab fa-twitch"
+                    ></i
+                    ><b>{{ player.social.twitch || '?' }}</b>
+                </span>
+            </transition>
+        </div>
     </div>
 </template>
 
@@ -56,5 +50,13 @@
     .fade-enter,
     .fade-leave-to {
         opacity: 0;
+    }
+
+    .nameplate {
+        background-color: #3a008b;
+        color: white;
+        height: 36px;
+        font-size: 24px;
+        width: 100%;
     }
 </style>
