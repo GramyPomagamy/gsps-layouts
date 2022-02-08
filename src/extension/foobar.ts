38545dba028fc97af8850ea100aa3db329c46a55
hoXyy <hoxiii@pm.me>
2022-02-08 19:58:22 +0100

    Add error handling to foobar code

--- src/extension/foobar.ts
+++ src/extension/foobar.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { get as nodecg } from './util/nodecg';
 import type { NodeCG } from 'nodecg/types/server';
-const log = new (nodecg() as NodeCG).Logger(`${nodecg().bundleName}:OBS`);
+const log = new (nodecg() as NodeCG).Logger(`${nodecg().bundleName}:foobar`);
 
 class FoobarControl {
     address: string;
@@ -17,6 +17,8 @@ class FoobarControl {
             headers: {
                 Accept: 'application/json',
             },
+        }).catch((error) => {
+            log.error('Błąd przy odciszaniu foobara: ' + error.message);
         });
     }
 
@@ -27,6 +29,8 @@ class FoobarControl {
             headers: {
                 Accept: 'application/json',
             },
+        }).catch((error) => {
+            log.error('Błąd przy wyciszaniu foobara: ' + error.message);
         });
     }
 
@@ -41,18 +45,23 @@ class FoobarControl {
     }
 
     async getSong(): Promise<string> {
-        const playerInfo: any = await axios.get(
-            `${this.address}/api/player?columns=${encodeURIComponent(
-                '%artist%,%title%'
-            )}`
-        );
+        try {
+            const playerInfo: any = await axios.get(
+                `${this.address}/api/player?columns=${encodeURIComponent(
+                    '%artist%,%title%'
+                )}`
+            );
 
-        if (
-            playerInfo.data.player.activeItem.columns[0] &&
-            playerInfo.data.player.activeItem.columns[1]
-        ) {
-            return `${playerInfo.data.player.activeItem.columns[0]} - ${playerInfo.data.player.activeItem.columns[1]}`;
-        } else {
+            if (
+                playerInfo.data.player.activeItem.columns[0] &&
+                playerInfo.data.player.activeItem.columns[1]
+            ) {
+                return `${playerInfo.data.player.activeItem.columns[0]} - ${playerInfo.data.player.activeItem.columns[1]}`;
+            } else {
+                return 'Brak piosenki';
+            }
+        } catch (error: any) {
+            log.error('Błąd otrzymywania piosenki: ' + error.message);
             return 'Brak piosenki';
         }
     }
