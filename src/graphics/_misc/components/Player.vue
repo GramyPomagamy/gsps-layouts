6f185e2d37a36bc2f21ad465a8b544574e6b776d
hoXyy <hoxiii@pm.me>
2022-02-22 21:04:09 +0100

    Tiny changes

--- src/graphics/_misc/components/Player.vue
+++ src/graphics/_misc/components/Player.vue
@@ -5,7 +5,6 @@
         <span
           v-if="cycle === 0"
           :style="{
-            'margin-top': '-2px',
             position: 'absolute',
             left: '0',
             width: '100%',
@@ -18,14 +17,14 @@
         <span
           v-if="cycle === 1"
           :style="{
-            'margin-top': '-2px',
+            marginTop: '0px',
             position: 'absolute',
             left: '0',
             width: '100%',
           }"
         >
           <i
-            :style="{ 'font-size': '20px', 'margin-right': '8px' }"
+            :style="{ 'font-size': '24px', float: 'left', marginTop: '5px', marginLeft: '10px' }"
             class="fab fa-twitch"
           ></i
           ><b>{{ player.social.twitch || '?' }}</b>
@@ -58,5 +57,7 @@
     height: 36px;
     font-size: 24px;
     width: 100%;
+    display: flex;
+    flex-direction: row;
   }
 </style>
