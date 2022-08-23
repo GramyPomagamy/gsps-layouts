1410fd8d6fd6b9c1ea2976b3ae5f91fb771f7c20
hoXyy <hoxiii@pm.me>
2022-08-23 22:29:45 +0200

    Don't show runners' twitch if one of the runners doesn't have twitch added

--- src/extension/layouts.ts
+++ src/extension/layouts.ts
@@ -1,21 +1,46 @@
-import { nameCycleReplicant } from './util/replicants';
+import { nameCycleReplicant, activeRunReplicant } from './util/replicants';
+import { RunDataActiveRun } from '../../../nodecg-speedcontrol/src/types/schemas';
 
 // Controls the name cycling ticks for players/hosts
 function cycleNames(reset = false): void {
   let cycle = 0;
-  if (!reset) {
-    cycle = nameCycleReplicant.value + 1;
+  if (doAllPlayersInRunHaveTwitch(activeRunReplicant.value)) {
+    if (!reset) {
+      cycle = nameCycleReplicant.value + 1;
+    }
+    if (cycle === 0) {
+      // Name
+      setTimeout(cycleNames, 45 * 1000);
+    } else if (cycle === 1) {
+      // Twitch
+      setTimeout(cycleNames, 15 * 1000);
+    } else {
+      cycleNames(true);
+      return;
+    }
   }
-  if (cycle === 0) {
-    // Name
-    setTimeout(cycleNames, 45 * 1000);
-  } else if (cycle === 1) {
-    // Twitch
-    setTimeout(cycleNames, 15 * 1000);
-  } else {
-    cycleNames(true);
-    return;
-  }
+
   nameCycleReplicant.value = cycle;
 }
+
+function doAllPlayersInRunHaveTwitch(run: RunDataActiveRun): boolean {
+  let players = [];
+  let playersWithTwitch = [];
+  for (let i = 0; i < run.teams.length; i++) {
+    let team = run.teams[i];
+    for (let i = 0; i < team.players.length; i++) {
+      players.push(team.players[i].name);
+      if (team.players[i].social.twitch) {
+        playersWithTwitch.push(team.players[i].social.twitch);
+      }
+    }
+  }
+
+  if (JSON.stringify(players) === JSON.stringify(playersWithTwitch)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 cycleNames(true);
