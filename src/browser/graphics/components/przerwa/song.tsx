4fc1cb3433921acbe700636e33ccf84bfd6493f2
Rafał Florczak <florczak.raf@gmail.com>
2025-02-13 22:14:38 +0100

    Add Song component to hosting, make it parametrized for easier reuse

--- src/browser/graphics/components/przerwa/song.tsx
+++ src/browser/graphics/components/przerwa/song.tsx
@@ -6,19 +6,19 @@ import { IconContext } from 'react-icons';
 import { IoIosMusicalNotes } from 'react-icons/io';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 
-const SongDiv = styled.div`
+const SongDiv = styled.div<{top: number, left: number}>`
   display: flex;
   position: fixed;
-  bottom: 110px;
-  right: 0px;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
   background: #e6e6e6;
   height: 32px;
   color: rgb(60, 60, 60);
-  width: 30%;
+  width: 576px;
   flex-direction: row;
   gap: 8px;
   font-size: 20px;
-  border-radius: 7px 0px 0px 7px;
+  border-radius: 7px 7px 7px 7px;
   align-content: space-between;
   justify-content: space-between;
 `;
@@ -28,13 +28,13 @@ const SongName = styled.div`
   margin-top: -10px;
 `;
 
-const Song = () => {
+const Song = ({ top = 50, left = 0 }) => {
   const [song] = useReplicant<Song>('song', '');
   const songRef = useRef(null);
 
   return (
     <IconContext.Provider value={{ size: '1.5em' }}>
-      <SongDiv>
+      <SongDiv top={top} left={left} >
         <IoIosMusicalNotes />
         <SwitchTransition mode="out-in">
           <CSSTransition
--- src/browser/graphics/odliczanie.tsx
+++ src/browser/graphics/odliczanie.tsx
@@ -3,12 +3,10 @@ import gspsLogo from './img/gsps_2024_logo.png';
 import mainBg from './img/main-background.png';
 import transparentBg from './img/gradient_transparent.png'
 import styled from 'styled-components';
-import { IoIosMusicalNotes } from 'react-icons/io';
-import { IconContext } from 'react-icons';
-import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import Song from './components/przerwa/song';
 import { useRef } from 'react';
 import { useReplicant } from 'use-nodecg';
-import { Countdown, CountdownRunning, Song } from 'src/types/generated';
+import { Countdown, CountdownRunning } from 'src/types/generated';
 import ThemeProvider from './components/theme-provider';
 
 const LayoutContainer = styled.div<{ useTransparentBackgrounds: boolean}>`
@@ -40,33 +38,10 @@ const Logo = styled.img`
   height: 45%;
 `;
 
-const SongDiv = styled.div`
-  display: flex;
-  position: fixed;
-  top: 80px;
-  background: #e6e6e6;
-  height: 32px;
-  color: rgb(60, 60, 60);
-  width: 25%;
-  flex-direction: row;
-  gap: 8px;
-  font-size: 20px;
-  border-radius: 0px 7px 7px 0px;
-  align-content: space-between;
-  justify-content: space-between;
-`;
-
-const SongName = styled.div`
-  width: 100%;
-  margin-top: -10px;
-`;
-
 export const Odliczanie = () => {
   const [countdown] = useReplicant<Countdown | undefined>('countdown', undefined);
   const [countdownRunning] = useReplicant<CountdownRunning>('countdownRunning', false);
-  const [song] = useReplicant<Song>('song', '');
   const [useTransparentBackgrounds] = useReplicant<boolean>('useTransparentBackgrounds', true);
-  const songRef = useRef(null);
   const countdownRef = useRef(null);
 
   return (
@@ -78,26 +53,7 @@ export const Odliczanie = () => {
         <CountdownDiv ref={countdownRef}>
           {countdown && countdownRunning && <span className="shadow">{countdown.formatted}</span>}
         </CountdownDiv>
-
-        <IconContext.Provider value={{ size: '1.5em' }}>
-          {' '}
-          <SongDiv>
-            <IoIosMusicalNotes />
-            <SwitchTransition mode="out-in">
-              <CSSTransition
-                key={song}
-                nodeRef={songRef}
-                appear
-                in={true}
-                timeout={1000}
-                classNames="fade">
-                <SongName ref={songRef} className="marquee">
-                  <p>{song}</p>
-                </SongName>
-              </CSSTransition>
-            </SwitchTransition>
-          </SongDiv>
-        </IconContext.Provider>
+        <Song top={80} left={0} />
       </LayoutContainer>
     </ThemeProvider>
   );
