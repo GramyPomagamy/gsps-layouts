32aeff6404a7f288bdf4eeecfa0753e4fbb1515e
Paweł Zatoka <github@hoxi.me>
2026-03-13 11:11:30 +0100

    fix: fix nodecg client types path
    apparently it changed between versions

--- src/types/augment-window.d.ts
+++ src/types/augment-window.d.ts
@@ -4,10 +4,11 @@
  * the NodeCG types, but allows us to automatically receive the configuration types.
  */
 
-import { NodeCGAPIClient } from 'node_modules/nodecg/out/client/api/api.client';
-import { Configschema } from './generated';
+import { NodeCGAPIClient } from "node_modules/nodecg/dist/dts/client/api/api.client";
+import { Configschema } from "./generated";
 
 declare global {
   let NodeCG: typeof NodeCGAPIClient;
   let nodecg: NodeCGAPIClient<Configschema>;
 }
+
